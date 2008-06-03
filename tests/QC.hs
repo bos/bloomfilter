99bc29eca15bbc61cdc284b1f174840d4ee7158b
Bryan O'Sullivan <bos@serpentine.com>
2008-06-02 21:06:21 -0700

    Fix QuickCheck properties

--- tests/QC.hs
+++ tests/QC.hs
@@ -1,26 +1,25 @@
 module Main where
 
 import Control.Monad (forM_)
-import Data.BloomFilter
-import Data.BloomFilter.Hash
-import Data.BloomFilter.Util
+import Data.BloomFilter.Easy (easyList, elemB)
+import Data.BloomFilter.Hash (Hashable(..))
 import qualified Data.ByteString.Char8 as SB
 import qualified Data.ByteString.Lazy.Char8 as LB
-import Data.Int
-import Data.Word
+import Data.Int (Int8, Int16, Int32, Int64)
+import Data.Word (Word8, Word16, Word32, Word64)
 import System.Environment (getArgs)
-import System.IO
+import System.IO (BufferMode(..), hSetBuffering, stdout)
 import Test.QuickCheck (Testable)
 
 import QCSupport
 
 prop_pai :: (Hashable a) => a -> (a, P) -> Bool
-prop_pai _ (xs, P p) = let bf = easyList p [xs]
-                                      in xs `elemB` bf
+prop_pai _ (xs, P q) = let bf = easyList q [xs] in xs `elemB` bf
 
 p :: Testable a => String -> a -> (Int -> IO (), String)
 p desc prop = (\count -> limCheck count prop, desc)
 
+tests :: [(Int -> IO (), String)]
 tests = [
    p "()" $ prop_pai ()
  , p "Bool" $ prop_pai (undefined :: Bool)
